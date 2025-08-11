c28e1826f399d0d75c28d0d210df7f5a6134a054
bbdolphin11 <caheocongnghe@gmail.com>
2025-08-11 13:14:09 +0700

    Update QLDanhba.cs

--- UI/QLDanhba.cs
+++ UI/QLDanhba.cs
@@ -1,1 +1,17 @@
 
+using AppDanhBa.BLL;
+using AppDanhBa.Models;
+
+class Program
+{
+    static void Main()
+    {
+        ContactBLL bll = new ContactBLL();
+        List<Contact> contacts = bll.GetContacts();
+
+        foreach (var c in contacts)
+        {
+            Console.WriteLine($"{c.Id} - {c.Name} - {c.Phone} - {c.Email}");
+        }
+    }
+}
