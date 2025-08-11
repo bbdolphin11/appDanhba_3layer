50d9f26f846ea160c13817271223f6b1926ad05e
bbdolphin11 <caheocongnghe@gmail.com>
2025-08-11 13:11:41 +0700

    Update contact.cs

--- Models/contact.cs
+++ Models/contact.cs
@@ -1,1 +1,10 @@
-
+namespace AppDanhBa.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
