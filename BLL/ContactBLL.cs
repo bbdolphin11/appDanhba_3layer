34c18bd9d10274015fe7eba7bd6d94a65d7447ae
bbdolphin11 <caheocongnghe@gmail.com>
2025-08-11 13:12:56 +0700

    Update ContactBLL.cs

--- BLL/ContactBLL.cs
+++ BLL/ContactBLL.cs
@@ -1,1 +1,16 @@
+using AppDanhBa.DAL;
+using AppDanhBa.Models;
 
+namespace AppDanhBa.BLL
+{
+    public class ContactBLL
+    {
+        private ContactDAL dal = new ContactDAL();
+
+        public List<Contact> GetContacts()
+        {
+            // Có thể thêm logic kiểm tra dữ liệu ở đây
+            return dal.GetAllContacts();
+        }
+    }
+}
